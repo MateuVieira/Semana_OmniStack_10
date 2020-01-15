61bfb4e67fd78ba0faafea7439dfe8b2aae8132a
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2020-01-15 20:28:00 -0300

    Update App.js
    Criando o layout do site e importando Css

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './css/global.css';
+import './css/App.css';
+import './css/Sidebar.css';
+import './css/Main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app'>
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className='input-block'>
+            <label htmlFor='github_username' >Usuário do GitHub</label>
+            <input name='github_username' id='github_username' required></input>
+          </div>
+          <div className='input-block'>
+            <label htmlFor='techs' >Tecnologias</label>
+            <input name='techs' id='techs' required></input>
+          </div>
+          <div className="input-group">
+            <div className='input-block'>
+              <label htmlFor='latitude' >Latitude</label>
+              <input name='latitude' id='latitude' required></input>
+            </div>
+            <div className='input-block'>
+              <label htmlFor='longitude' >Longitude</label>
+              <input name='longitude' id='longitude' required></input>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className='dev-item'>
+            <header>
+              <img src='https://avatars0.githubusercontent.com/u/2254731?s=460&v=4' alt='Diego Fernandes' />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.JS</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href='https://github.com/diego3g' >Acessar perfil no GitHub</a>
+          </li>
+
+          <li className='dev-item'>
+            <header>
+              <img src='https://avatars0.githubusercontent.com/u/2254731?s=460&v=4' alt='Diego Fernandes' />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.JS</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href='https://github.com/diego3g' >Acessar perfil no GitHub</a>
+          </li>
+
+          <li className='dev-item'>
+            <header>
+              <img src='https://avatars0.githubusercontent.com/u/2254731?s=460&v=4' alt='Diego Fernandes' />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.JS</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href='https://github.com/diego3g' >Acessar perfil no GitHub</a>
+          </li>
+
+          <li className='dev-item'>
+            <header>
+              <img src='https://avatars0.githubusercontent.com/u/2254731?s=460&v=4' alt='Diego Fernandes' />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.JS</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href='https://github.com/diego3g' >Acessar perfil no GitHub</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
