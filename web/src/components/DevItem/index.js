b1e1b0498a1652058eafc1162531b0410a975147
MateuVieira <35344433+MateuVieira@users.noreply.github.com>
2020-01-16 13:15:45 -0300

    Update

--- web/src/components/DevItem/index.js
+++ web/src/components/DevItem/index.js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from '../../servers/api';
 
 import './styles.css';
 
 function DevItem({ dev }) {
-    return (
-        <li className='dev-item'>
-        <header>
-          <img src={dev.avatar_url} alt={dev.name} />
-          <div className="user-info">
-            <strong>{dev.name}</strong>
-            <span>{dev.techs.join(', ')}</span>
-          </div>
-        </header>
-        <p>{dev.bio}</p>
-        <a href={`https://github.com/${dev.github_username}`} >Acessar perfil no GitHub</a>
-      </li>
-    );
+
+  // const [edit, setEdit] = useState(false);
+
+
+  async function handleDeleteDev() {
+
+    if (
+      window.confirm(
+        `Voce tem certeza que deseja excluir o usuário ${dev.name}`
+      )
+    ) {
+      try {
+        await api.delete(`/devs/${dev.github_username}`);
+        window.location.reload();
+      } catch {
+        return;
+      }
+    }
+  }
+
+  async function handleUpdateDev() {
+   
+    
+  }
+
+  return ( 
+
+    <li className='dev-item'>
+      <header>
+        <img src={dev.avatar_url} alt={dev.name} />
+        <div className="user-info">
+          <strong>{dev.name}</strong>
+          <span>{dev.techs.join(', ')}</span>
+        </div>
+      </header>
+      <p>{dev.bio}</p>
+      <a href={`https://github.com/${dev.github_username}`} >Acessar perfil no GitHub</a>
+
+      {/* Add Update and Delete */}
+
+      <div className="button-group">
+        <button className='button-edit' onClick={handleUpdateDev} ></button>
+        <button className='button-close' onClick={handleDeleteDev} ></button>
+      </div>
+
+      {/* End */}
+    </li>
+  );
 }
 
 export default DevItem;
